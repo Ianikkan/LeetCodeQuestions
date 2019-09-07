819c1896a9ca00ef864338955d66126144b3a5c9
Iannikan <imniklason@gmail.com>
2019-09-07 15:39:53 -0600

    236, lowest common ancestor of a binary tree, python iterative

--- lowestCommonAncestorBianryTree/solution.py
+++ lowestCommonAncestorBianryTree/solution.py
@@ -1,10 +1,11 @@
  #Definition for a binary tree node.
- class TreeNode:
+class TreeNode:
      def __init__(self, x):
          self.val = x
          self.left = None
          self.right = None
-
+"""
+#Recursive
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         self.ans = None
@@ -26,4 +27,34 @@ class Solution:
 
         helper(root)
         return self.ans
+"""
+
+#Iterative 
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        stack = []
+        parents = {root: None}
+        stack.append(root)
+        while len(stack) > 0 and (q not in parents or p not in parents) :
+            cur = stack.pop()
+            if cur.left:
+                stack.append(cur.left)
+                parents[cur.left] = cur
+            if cur.right:
+                stack.append(cur.right)
+                parents[cur.right] = cur
+                
+        path = [q]
+        qUp = parents[q]
+        while qUp is not None:
+            path.append(qUp)
+            qUp = parents[qUp]
+        
+        pUp = p
+        while pUp is not None:
+            if pUp in path:
+                return pUp
+            pUp = parents[pUp]
             
+        
