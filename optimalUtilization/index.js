d129d46db6e6ac36213be7a0c6a4874fd9b3ce07
Iannikan <imniklason@gmail.com>
2019-10-02 12:21:35 -0600

    npm stuff

--- optimalUtilization/index.js
+++ optimalUtilization/index.js
@@ -1,35 +1,36 @@
 // current solution may have a problem with duplicate values
-var utilizeOptimally = (a, b, target) => {
-    a.sort((a , b) =>  a[1] - b[1])
-    
+var optimize = function(a,b,target){
+    a.sort((a,b) => {
+        return a[1] - b[1]
+    })
+    b.sort((a,b) => {
+        return a[1] - b[1]
+    })
 
-    b.sort((a, b) =>  a[1] - b[1])
-    result = []
+    let ap = 0, bp = b.length - 1
+    let bestPairs = []
+    let bestPairSum =  Number.MIN_SAFE_INTEGER;
 
-    let aPointer = 0
-    let bPointer = b.length - 1
-    let best = null
+    while (ap < a.length && bp >= 0){
+        let testSum = a[ap][1] + b[bp][1]
+        if (testSum <= target &&(bestPairs.length == 0 || (testSum >= bestPairSum  ))){
+            if (testSum > bestPairSum) {
+              bestPairs = [];
+              bestPairSum = testSum
+            }
+            bestPairs.push([a[ap][0],b[bp][0]])
+            m
+        }
+        if (testSum > target){
+            bp--
+        }
+        else {
+            ap++
+        }
 
-    while(aPointer < a.length && bPointer >= 0){
-        curr = a[aPointer][1] + b[bPointer][1]
-        if(curr === best){
-            result.push([a[aPointer][0], b[bPointer][0]])
-        }
-        if (curr > best && curr <= target){
-            best = curr
-            result = [a[aPointer][0], b[bPointer][0]]
-        }
-
-        if (curr > target){
-            bPointer --;
-        }
-        else{
-            aPointer ++
-        }
     }
+    return bestPairs
 
-    return result
+
 }
-
-
-utilizeOptimally([[1, 3], [2, 5], [3, 7], [4, 10]],[[1, 2], [2, 3], [3, 4], [4, 5]], 10 )
+optimize([[1, 3], [2, 5], [3, 7], [4, 10]],[[1, 2], [2, 3], [3, 4], [4, 5]], 10 )
